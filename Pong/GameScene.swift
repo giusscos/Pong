f1b34dec92c8ccf9d898056115497767e7b738ed
giusscos <giu.cosenza08@gmail.com>
2024-12-09 23:51:27 +0100

    Add score logic

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -10,13 +10,17 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var impulseUnit = 20
+    var impulseUnit = 50
     
     var ball = SKSpriteNode()
     var cpu = SKSpriteNode()
     var player = SKSpriteNode()
     
+    var score = [Int]()
+    
     override func didMove(to view: SKView) {
+        startGame()
+        
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         cpu = self.childNode(withName: "cpu") as! SKSpriteNode
         player = self.childNode(withName: "player") as! SKSpriteNode
@@ -31,7 +35,50 @@ class GameScene: SKScene {
         self.physicsBody = border
     }
     
+    func startGame(){
+        score = [0,0]
+    }
+    
+    func addScore(playerWon: SKSpriteNode) {
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        
+        if playerWon == player {
+            score[0] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: impulseUnit, dy: impulseUnit))
+        } else if playerWon == cpu {
+            score[1] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: (impulseUnit * -1), dy: (impulseUnit * -1)))
+        }
+        
+        print(score)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            player.run(SKAction.moveTo(x: location.x, duration: 0.1))
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            player.run(SKAction.moveTo(x: location.x, duration: 0.1))
+        }
+    }
+    
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+        cpu.run(SKAction.moveTo(x: ball.position.x, duration: 0.5))
+        
+        if ball.position.y <= (player.position.y - player.size.height) {
+            addScore(playerWon: cpu)
+        } else if ball.position.y >= (cpu.position.y + cpu.size.height) {
+            addScore(playerWon: player)
+        }
     }
 }
